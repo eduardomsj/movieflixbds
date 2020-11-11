ad5e8fa8e34ccf3498845a60454b31c61c860c87
eduardomsj <eduardomsj@gmail.com>
2020-11-11 19:59:22 -0300

    Project created

---
+++ backend/src/main/java/com/eduardojr/movieflixbds/MovieflixbdsApplication.java
@@ -0,0 +1,13 @@
+package com.eduardojr.movieflixbds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieflixbdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieflixbdsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/eduardojr/movieflixbds/MovieflixbdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardojr.movieflixbds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieflixbdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
