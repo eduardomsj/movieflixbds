a4e33d0072dc560cead8e4cf05187e17da9e6e41
eduardomsj <eduardomsj@gmail.com>
2021-02-21 19:50:58 -0300

    Created genre filtering

---
+++ front-web/src/pages/Catalog/components/FilterMovies/index.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import Select from 'react-select';
+import { Genre } from '../../../../core/types/movies';
+import { makePrivateRequest } from '../../../../core/utils/request';
+import './styles.scss';
+
+export type FilterForm = {
+    genreId?: number;
+}
+
+type Props = {
+    onSearch: (filter: FilterForm) => void;
+}
+
+const FilterMovies = ({ onSearch }: Props) => {
+
+    const [genres, setGenres] = useState<Genre[]>([]);
+    const [genre, setGenre] = useState<Genre>();
+
+    useEffect(() => {      
+        makePrivateRequest({ url: '/genres' })
+            .then(response => setGenres(response.data))
+    }, [])
+
+    const handleChangeGenre = (genre: Genre) => {
+        setGenre(genre);
+        onSearch({ genreId: genre?.id })
+        //getMovies({ genreId: genre?.id })
+    }
+
+    return (
+        <div className="catalog-container">
+            <div className="catalog-content-search">
+                <Select 
+                    name="genres"
+                    key={`select-${genre?.id}`}
+                    value={genre}
+                    options={genres}
+                    getOptionLabel={(option: Genre) => option.name}
+                    getOptionValue={(option: Genre) => String(option.id)}
+                    placeholder="Gêneros"
+                    inputId="genres"
+                    onChange={value => handleChangeGenre(value as Genre)}
+                    classNamePrefix="catalog-select"
+                />
+            </div>
+        </div>
+    );
+}
+
+export default FilterMovies;
--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -8,6 +8,7 @@ import { makePrivateRequest } from '../../core/utils/request';
 import CardMovies from './components/CardMovies';
 import ButtonLogout from '../../core/components/ButtonLogout';
 import Navbar from '../../core/components/Navbar';
+import FilterMovies, { FilterForm } from './components/FilterMovies'
 import Pagination from '../../core/components/Pagination';
 import './styles.scss';
 
@@ -17,11 +18,12 @@ const Catalog = () => {
     const [isLoading, setIsLoading] = useState(false);
     const [activePage, setActivePage] = useState(0);
     
-    const getMovies = useCallback(() => {
+    const getMovies = useCallback((filter?: FilterForm) => {
         
         const params = {
             page: activePage,
             size: 4,
+            genreId: filter?.genreId
         }
 
         setIsLoading(true);
@@ -43,13 +45,14 @@ const Catalog = () => {
                 <ButtonLogout />
             </Navbar>
             <div className="catalog-container">
+                <FilterMovies onSearch={filter => getMovies(filter)} />
                 <div className="catalog-container-card">                    
                     {moviesResponse?.content.map(movie => (
                         <Link to={`/movies/${movie.id}`}>
                             <CardMovies movie={movie} key={movie.id} />
                         </Link>
-                        ))
-                    })
+                        )
+                    )}
                 </div>
             </div>
             {moviesResponse && <Pagination
